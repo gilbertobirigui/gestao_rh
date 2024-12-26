b7f9370dbf890786baed0526d5db8d338f83e3d3
gilbertoBirigui <gfilmesbirigui@hotmail.com>
2024-12-26 13:57:57 -0300

    Criando apps Funcionario

---
+++ apps/empresas/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Empresa(models.Model):
+    name = models.CharField(max_length=100, help_text='Nome da Empresa', verbose_name='nome', default='Default Name')
+
+
+
+    def __str__(self):
+        return self.name
