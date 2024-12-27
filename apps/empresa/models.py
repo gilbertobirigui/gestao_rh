14deab9db37bedd6db2351f6e52ff75b026d5f5c
gilbertoBirigui <gfilmesbirigui@hotmail.com>
2024-12-26 23:03:43 -0300

    configuracao de relacionamento entre models

--- apps/empresa/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class Empresa(models.Model):
-    name = models.CharField(max_length=100, help_text='Nome da Empresa', verbose_name='nome', default='Default Name')
--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 # Create your models here.
 
 class Funcionario(models.Model):
     name = models.CharField(max_length=60, verbose_name= 'nome')
-
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete = models.PROTECT)
 
     def __str__(self):
         return self.name
