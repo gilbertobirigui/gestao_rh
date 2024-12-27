3f0cae9b00a08b38a6fbef2820126879b54636f7
gilbertoBirigui <gfilmesbirigui@hotmail.com>
2024-12-26 21:18:53 -0300

    criando a aplicacao Departamentos

---
+++ apps/departamentos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Departamento(models.Model):
+    name = models.CharField(max_length=70, verbose_name='nome')
+
+
+    def __str__(self):
+        return self.name
+    
+
