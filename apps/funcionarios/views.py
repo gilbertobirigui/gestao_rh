3a39c8733b2261ab5587cc6ee1dd5395da1b1619
gilbertoBirigui <gfilmesbirigui@hotmail.com>
2024-12-29 19:28:02 -0300

    crud funcionarios, delete

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,20 +1,27 @@
 
-from django.views.generic import ListView, UpdateView
+from django.views.generic import ListView, UpdateView, DeleteView
 from .models import Funcionario
+from django.urls import reverse_lazy
 
 
 class FuncionariosList(ListView):
     model = Funcionario
-
-
-
-    def get_queryset(self):
-        empresa_logada = self.request.user.funcionario.empresa
-        queryset = Funcionario.objects.filter(empresa = empresa_logada)
-            
-        return queryset
+    context_object_name = 'funcionarios'
     
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa if hasattr(self.request.user, 'funcionario') else None
+        if empresa_logada:
+            queryset = Funcionario.objects.filter(empresa=empresa_logada)
+        else:
+            queryset = Funcionario.objects.none()  # Retorna um queryset vazio se o usuário não tiver um funcionário associado
+        return queryset
 
 class FuncionarioEdit(UpdateView):
     model = Funcionario
     fields = ['name', 'departamentos']
+
+
+class FuncionarioDelete(DeleteView):
+    model = Funcionario
+    success_url = reverse_lazy('list_funcionarios')
+
